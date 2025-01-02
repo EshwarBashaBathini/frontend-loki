77e7731adcb4958e42faea3df25764a5ccf6a4ba
Eshwar Basha <MTL1022@middlewaretalents.in>
2025-01-02 12:27:46 +0530

    second Commit

--- src/EmployeeComponents/DocumentUploadForm.jsx
+++ src/EmployeeComponents/DocumentUploadForm.jsx
@@ -58,7 +58,7 @@ const DocumentUploadForm = ({ formData, onNext, onBack, onCancel, onFormDataChan
         });
 
         try {
-            const response = await axios.post('http://52.176.99.186:8085/api/v1/employeeManager/add', formDataToSend, {
+            const response = await axios.post('http://localhost:8085/api/v1/employeeManager/add', formDataToSend, {
                 headers: {
                     'Content-Type': 'multipart/form-data',
                 }
