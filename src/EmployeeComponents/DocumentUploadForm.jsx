c818041f417601625ec7873cd0e19cdbf3f367e0
EshwarBashaBathini <160468621+EshwarBashaBathini@users.noreply.github.com>
2025-02-05 11:29:51 +0530

    Update DocumentUploadForm.jsx

--- src/EmployeeComponents/DocumentUploadForm.jsx
+++ src/EmployeeComponents/DocumentUploadForm.jsx
@@ -58,7 +58,7 @@ const DocumentUploadForm = ({ formData, onNext, onBack, onCancel, onFormDataChan
         });
 
         try {
-            const response = await axios.post('http://52.228.61.110:8085/api/v1/employeeManager/add', formDataToSend, {
+            const response = await axios.post('https://eshwar-test-01.azurewebsites.net/api/v1/employeeManager/add', formDataToSend, {
                 headers: {
                     'Content-Type': 'multipart/form-data',
                 }
