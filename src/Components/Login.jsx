13dcc22b8d7f8b83263062997dff73aefee292b3
EshwarBashaBathini <160468621+EshwarBashaBathini@users.noreply.github.com>
2025-02-06 09:49:41 +0530

    Update Login.jsx

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -126,7 +126,7 @@ const Login = () => {
             </main>
             <div className="welcome-container">
                 <h1 className="heading-secondary">
-                    Welcome to <span className="lg">eash Buddy!</span>
+                    Welcome to <span className="lg">MTL Buddy!</span>
                 </h1>
             </div>
         </div>
